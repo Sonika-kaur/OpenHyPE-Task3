e2b98966c3a54cea0db6e7ed5b85e71e0642a7ef
Sonika-kaur <129415576+Sonika-kaur@users.noreply.github.com>
2023-03-30 18:17:39 +0200

    Add files via upload

---
+++ credential_temp.py
@@ -0,0 +1,10 @@
+# Replace your database credentials with each below parameters
+
+
+PGHOST = "localhost"
+PGDATABASE = "postgres"
+PGUSER = "postgres"
+PGPASSWORD = "password"
+PORT = "5432"
+URL = "postgresql://" + PGUSER + ":" + PGPASSWORD + \
+    "@" + PGHOST + ":" + PORT + "/" + PGDATABASE
